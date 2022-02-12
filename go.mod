b3bdbc04a60717969906b0757d1925e9c0cab38a
acse-sm321 <shuheng.mo21@imperial.ac.uk>
2022-02-12 01:34:59 +0000

    Working on database operations

---
+++ go.mod
@@ -0,0 +1,21 @@
+module IM-gossip-space
+
+go 1.17
+
+require (
+	gorm.io/gorm v1.22.5
+	xorm.io/xorm v1.2.5
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/goccy/go-json v0.7.4 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	xorm.io/builder v0.3.9 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"html/template"
 	"log"
 	"net/http"
+	"xorm.io/xorm"
 )
 
 func user_login(writer http.ResponseWriter, request *http.Request) {
@@ -30,8 +34,6 @@ func user_login(writer http.ResponseWriter, request *http.Request) {
 		data["token"] = "test"
 		Respond(writer, 0, data, "")
 	}
-
-	//io.WriteString(writer, "Hello world!!") // test hello world
 }
 
 // define a struct to store the header information
@@ -39,6 +41,22 @@ type H struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data,omitempty"`
+}
+
+var DbEngine *xorm.Engine
+
+func init() {
+	drivername := "mysql"
+	DsName := "root:root@(127.0.0.1:3306)/chat?charset=utf8"
+	DbEngine, err := xorm.NewEngine(drivername, DsName)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	DbEngine.ShowSQL(true)      // show sql
+	DbEngine.SetMaxOpenConns(2) // set max conn count
+	// automatic user constructor
+	//DbEngine.Sync2(new(User))
+	fmt.Println("Init database OK")
 }
 
 func Respond(w http.ResponseWriter, code int, data interface{}, msg string) {
@@ -57,15 +75,29 @@ func Respond(w http.ResponseWriter, code int, data interface{}, msg string) {
 	w.Write([]byte(ret))
 }
 
+func RegisterView() {
+	// note the directory here
+	tpl, err := template.ParseGlob("view/**/*")
+	if nil != err {
+		log.Fatal(err.Error())
+	}
+	for _, v := range tpl.Templates() {
+		tplname := v.Name()
+		http.HandleFunc(tplname, func(writer http.ResponseWriter, request *http.Request) {
+			tpl.ExecuteTemplate(writer, tplname, nil)
+		})
+	}
+}
+
 func main() {
 	//bind the request and handler function
 	http.HandleFunc("/user/login", user_login)
 
 	//provide static resource support: File server
-	//http.Handle("/", http.FileServer(http.Dir(".")))
-
-	//provide designated directory
+	//provide designated directory for encapsulation
 	http.Handle("/asset/", http.FileServer(http.Dir(".")))
+
+	RegisterView()
 
 	http.ListenAndServe(":8080", nil)
 }
